7669de34ab25ddb4b15720c2331ebe1e30fac190
ChingizPro <chingizprog@gmail.com>
2021-08-08 14:47:56 +0500

    Kollej vebsayti

---
+++ maktab/js.js
@@ -0,0 +1,4 @@
+alert("salom Surxondaryo viloyati, Jarqo'rg'on tumani, 1-son kasb-hunar maktabining saytiga xush kelibsiz");
+var a = prompt("ismingizni kiriting");
+var b = prompt("familiyangizni kiriting");
+alert("Bizning saytimizga xush kelibsiz " + a + " " + b )
